af81d96421cc18d494f4d87c5b7114dc421c8ef0
Simon Descarries <simon.descarries@vantrix.com>
2018-07-30 14:21:19 -0400

    Reviewed installation, fixed paths and everything

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM centos:7
 LABEL maintainer "simon.descarries@gmail.com"
 ENV ANDROID_HOME /opt/android
-
-# Config
-RUN usermod -s /bin/bash root
+ENV GRADLE_HOME  /opt/gradle
+ENV NODE_VERSION v10.7.0
+ENV PATH ${HOME}/.nvm/versions/node/${NODE_VERSION}/bin:${GRADLE_HOME}/bin:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${HOME}/.yarn/bin:${HOME}/.config/yarn/global/node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Update repository cache
 RUN yum makecache
--- setup-android.sh
+++ setup-android.sh
@@ -4,11 +4,23 @@
   [ ! -z "${ANDROID_HOME}" ] ||
   exit 1
 
+  [ ! -z "${GRADLE_HOME}" ] ||
+  exit 1
+
   mkdir -p ${ANDROID_HOME} &&
   wget -qO sdk-tools-linux.zip "https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip" &&
-  unzip sdk-tools-linux.zip &&
+  unzip -q sdk-tools-linux.zip &&
   rm -f sdk-tools-linux.zip &&
   mv -f ./tools ${ANDROID_HOME}/ ||
+  exit 1
+
+  rm -rf ${GRADLE_HOME} &&
+  mkdir -p $(dirname ${GRADLE_HOME}) &&
+  wget -qO gradle.zip "https://downloads.gradle.org/distributions/gradle-4.9-bin.zip" &&
+  unzip -q gradle.zip &&
+  rm -f gradle.zip &&
+  mv -f ./gradle-4.9 ${GRADLE_HOME} &&
+  ln -sf ${GRADLE_HOME}/bin/gradle /usr/bin/  ||
   exit 1
 
   echo -e 'y' |
@@ -19,5 +31,17 @@
     "build-tools;27.0.3" \
     "extras;android;m2repository" \
     "extras;google;google_play_services" \
-    "extras;google;m2repository"
+    "extras;google;m2repository" &> /dev/null ||
+  exit 1
+
+  cat >> ~/.bashrc << EOF
+
+export ANDROID_HOME=${ANDROID_HOME}
+export GRADLE_HOME=${GRADLE_HOME}
+
+export PATH=\${ANDROID_HOME}/tools:\${ANDROID_HOME}/platform-tools:\${PATH}
+export PATH=\${GRADLE_HOME}/bin:\${PATH}
+
+EOF
+
 }
